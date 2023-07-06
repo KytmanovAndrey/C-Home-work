8c79ffb92e1a2640b6f4538f0e03d7a981b7f3f2
KytmanovAndrey <kytmanova.m@yandex.ru>
2023-07-06 07:22:33 +0200

    4 tasks added

---
+++ Task 8/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число ");
+int a = int.Parse(Console.ReadLine());
+int b = 2;
+if (a>=b) 
+{
+    Console.Write(b);
+    while(a-2>=b)
+{
+    b = b + 2;
+    Console.Write(", " + b);
+}
+}
+b = 0;
+if (a-1<b)
+{
+    Console.Write(b);
+    while(a+1<b)
+{
+    b = b - 2;
+    Console.Write(", " + b);
+}
+}
